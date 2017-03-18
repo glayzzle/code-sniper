ccfa8646c031045e10fd14870817c0e903103f5d
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-03-18 18:22:50 +0100

    improve test on parsing errors

--- test/visitor.js
+++ test/visitor.js
@@ -20,6 +20,12 @@ describe('Test the visitor API', function() {
   it('test errors', function() {
     var ast = scan.parser.parseCode('<?php echo ?!?', 'foo.php');
     ast.kind.should.be.exactly('program');
-    scan.report.getMessages('foo.php').length.should.be.exactly(1);
+    var messages = scan.report.getMessages('foo.php');
+    messages.length.should.be.exactly(1);
+    messages[0].level.should.be.exactly('critical');
+    messages[0].text.should.be.exactly('Parse Error : syntax error, unexpected \'?\' on line 1');
+    messages[0].location.start.line.should.be.exactly(1);
+    messages[0].location.end.line.should.be.exactly(1);
+    messages[0].rule.namespace.should.be.exactly('parser');
   });
 });
